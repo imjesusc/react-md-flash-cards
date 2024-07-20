a34ba512f19e67b06211d7470614052de6030fda
imjesusc <iamjesus.ds@gmail.com>
2024-07-20 18:42:41 -0400

    feat: Add ui components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,72 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  prefix: "",
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        "2xl": "800px",
+      },
+    },
+    extend: {
+      boxShadow: {
+        medium:
+          "0px 0px 15px 0px rgba(0,0,0,.03),0px 2px 30px 0px rgba(0,0,0,.08),0px 0px 1px 0px rgba(0,0,0,.3)",
+      },
+      fontFamily: {
+        inter: ["var(--font-inter)", "sans-serif"],
+      },
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
+    screens: {
+      phone: "414px",
+      mobile: "640px",
+      tablet: "768px",
+      tabletlg: "960px",
+      tabletxl: "1024px",
+      laptop: "1200px",
+      laptoplg: "1400px",
+      desktop: "1700px",
+    },
   },
-  plugins: [],
 };
