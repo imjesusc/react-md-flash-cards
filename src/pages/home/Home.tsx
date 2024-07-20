bd7498e26a0a2068895f1c936faa264355838be1
imjesusc <iamjesus.ds@gmail.com>
2024-07-20 18:43:20 -0400

    feat: Add home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,12 @@
+import { CollectionCardForm } from "./components/collection-card-form";
+
 export default function Home() {
   return (
-    <main>
-      <h1>React Markdown Flash Card</h1>
+    <main className="my-4 mx-2 tablet:mx-auto max-w-[700px] flex flex-col gap-4">
+      <header>
+        <h1 className="text-2xl text-semibold">Add collections card</h1>
+      </header>
+      <CollectionCardForm />
     </main>
   );
 }
