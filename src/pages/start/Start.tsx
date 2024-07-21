868d0730800d64ca3f9c24826d4530b5d3ce9a4a
imjesusc <iamjesus.ds@gmail.com>
2024-07-20 22:00:00 -0400

    feat: add start page component

--- src/pages/start/Start.tsx
+++ src/pages/start/Start.tsx
@@ -1,60 +1,89 @@
+import MarkdownPreview from "@uiw/react-markdown-preview";
 import { useState } from "react";
 import { Button } from "../../components/button";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "../../components/tabs";
+import { useCards } from "../../hooks/use-cards";
 import { useCardsStore } from "../../store/cards.store";
-import MarkdownPreview from "@uiw/react-markdown-preview";
 import { cn } from "../../utilities/cn";
+import { ActionsButtons } from "./components/actions-buttons";
+import { FinishPanel } from "./components/finish-panel";
 
 export function Start() {
-  const [tabs, setTabs] = useState<"GENERAL" | "CODE">("GENERAL");
-  const handleSetTabs = (tab: "GENERAL" | "CODE") => {
-    setTabs(tab);
+  const [tab, setTab] = useState("general");
+  const onTabChange = (value: string) => {
+    setTab(value);
   };
 
   const currentCard = useCardsStore((state) => state.currentCard);
+  const { showNextCard, changeStatus } = useCards();
+
   return (
     <main className="my-4 mx-2 tablet:mx-auto max-w-[700px] flex flex-col gap-4">
-      <header className="flex m-auto border rounded-lg w-max">
-        <Button
-          className="rounded-r-none"
-          onClick={() => handleSetTabs("GENERAL")}
-          variant={tabs === "GENERAL" ? "default" : "secondary"}
-        >
-          General
-        </Button>
-        <Button
-          className="rounded-l-none"
-          onClick={() => handleSetTabs("CODE")}
-          variant={tabs === "CODE" ? "default" : "secondary"}
-        >
-          Code
-        </Button>
-      </header>
+      {currentCard ? (
+        <>
+          <Tabs value={tab} onValueChange={onTabChange} className="m-auto">
+            <TabsList className="flex justify-center">
+              <TabsTrigger value="general" asChild>
+                <Button
+                  className="rounded-r-none pointer-events-none"
+                  variant={tab === "general" ? "default" : "ghost"}
+                >
+                  General
+                </Button>
+              </TabsTrigger>
+              <TabsTrigger value="code" asChild>
+                <Button
+                  className="rounded-l-none pointer-events-none"
+                  variant={tab === "code" ? "default" : "ghost"}
+                >
+                  Code
+                </Button>
+              </TabsTrigger>
+            </TabsList>
 
-      {tabs === "GENERAL" && (
-        <div
-          className={cn(
-            "flex justify-center items-center text-2xl p-4 border m-auto",
-            "tablet:w-[400px]  rounded-lg h-[300px] text-center",
-          )}
-        >
-          <h2>{currentCard?.general}</h2>
-        </div>
-      )}
-      {tabs === "CODE" && (
-        <div
-          className={cn(
-            "h-[300px] p-2 border rounded-lg overflow-y-scroll",
-            " tablet:w-[400px] m-auto ",
-          )}
-        >
-          <MarkdownPreview
-            className="w-full"
-            source={currentCard?.code}
-            wrapperElement={{
-              "data-color-mode": "light",
-            }}
+            <TabsContent value="general">
+              <div
+                className={cn(
+                  "flex justify-center items-center text-2xl p-4 border m-auto",
+                  "tablet:w-[500px] rounded-lg h-[350px] text-center"
+                )}
+              >
+                <p>{currentCard?.general}</p>
+              </div>
+            </TabsContent>
+            <TabsContent value="code">
+              <div
+                className={cn(
+                  "h-[350px] p-2 border rounded-lg overflow-y-scroll",
+                  "tablet:w-[500px] m-auto"
+                )}
+              >
+                <MarkdownPreview
+                  className="w-full"
+                  source={currentCard?.code}
+                  wrapperElement={{
+                    "data-color-mode": "light",
+                  }}
+                />
+              </div>
+            </TabsContent>
+          </Tabs>
+
+          <ActionsButtons
+            onTabChange={onTabChange}
+            tab={tab}
+            currentCard={currentCard}
+            checkStatus={changeStatus}
+            getNextCard={showNextCard}
           />
-        </div>
+        </>
+      ) : (
+        <FinishPanel />
       )}
     </main>
   );
